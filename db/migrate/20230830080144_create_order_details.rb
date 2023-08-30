5fce020319b2cf8bc09417b4ed46e609a0cc5fd8
mion1213 <141290890+mion1213@users.noreply.github.com>
2023-08-30 09:47:32 +0000

    [Stop]顧客:会員登録実装前

---
+++ db/migrate/20230830080144_create_order_details.rb
@@ -0,0 +1,13 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+      t.integer :item_id, null: false
+      t.integer :order_id, null: false
+      t.integer :price_including_tax, null: false
+      t.integer :quantity, null: false
+      t.integer :crafting_status, null: false
+
+      t.timestamps
+    end
+  end
+end
